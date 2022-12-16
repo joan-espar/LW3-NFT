2bd6e227f05ad73b96af53f63eeecd845d322b18
joan <joan.espar@gmail.com>
2022-12-16 13:38:36 +0100

    changes

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -248,8 +248,8 @@ export default function Home() {
   // In this case, whenever the value of `walletConnected` changes - this effect will be called
   useEffect(() => {
     // if wallet is not connected, create a new instance of Web3Modal and connect the MetaMask wallet
-    //if (!walletConnected) { // original
-    if (!walletConnected && false) { // test per veure si aixo dona problemes
+    if (!walletConnected) { // original
+    // if (!walletConnected && false) { // test per veure si aixo dona problemes
       // Assign the Web3Modal class to the reference object by setting it's `current` value
       // The `current` value is persisted throughout as long as this page is open
       web3ModalRef.current = new Web3Modal({
